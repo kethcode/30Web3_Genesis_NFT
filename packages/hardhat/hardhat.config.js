a5eb712d4e8e37984273ceab7c4364146086e68b
BROCHARD, Xavier Alexandre <xavier-alexandre.brochard@airbus.com>
2022-02-01 00:35:57 +0100

    Use dedicated Infura ID

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -100,7 +100,7 @@ module.exports = {
     // },
 
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://rinkeby.infura.io/v3/73903f4fb1924d5baac7343f23e67c63", // <---- YOUR INFURA ID! (or it won't work)
 
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
 
